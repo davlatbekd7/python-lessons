49255d4b8b20aec7ae2145e885acb79466230a58
Davlatbek <82755499+davlatbekd7@users.noreply.github.com>
2021-04-18 15:39:28 +0500

    Create list.py

---
+++ list.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 17 10:59:41 2021
+
+@author: Davlatbek
+"""
+num=float(input("Istalgan sonni kiriting: "))
+if num>0:
+    print(num**(1/2))
+else:
+    print("musbat son kiriting")
+
+    
